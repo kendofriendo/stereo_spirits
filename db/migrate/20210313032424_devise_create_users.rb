97a0afb201d9f0be60201bef053c6665993b31ab
kendofriendo <goadke@g.cofc.edu>
2021-03-12 22:30:18 -0500

    generate controller for index page, add route and added flash placeholders per devise instructions

--- db/migrate/20210313032424_devise_create_users.rb
+++ db/migrate/20210313032424_devise_create_users.rb
@@ -4,8 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "email"
+      t.string :username, optional: true, default: "username"
+      t.string :encrypted_password, null: false, default: "password"
 
       ## Recoverable
       t.string   :reset_password_token

ff7077b593082addcbb5d5d385aef8e63236bd7f
kendofriendo <goadke@g.cofc.edu>
2021-03-17 20:22:53 -0400

    delete example text

--- db/migrate/20210317124807_add_attributes_to_users.rb
+++ db/migrate/20210317124807_add_attributes_to_users.rb
@@ -2,6 +2,6 @@ class AddAttributesToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :username, :string
     add_index :users, :username, :string, :unique => true
-    #Ex:- add_index("admin_users", "username")
+    
   end
 end

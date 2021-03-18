7f2ba9f4d67c720c4fdc6a2059e61e4d6a1ea154
kendofriendo <goadke@g.cofc.edu>
2021-03-18 13:56:11 -0400

    edit navbar for user sessions remove add_attr mig for now

--- db/migrate/20210317124807_add_attributes_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :username, :string
-    add_index :users, :username, :string, :unique => true
-    
-  end
-end

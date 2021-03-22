a072923dd345cc95afe721cbc6986dc69ef58bb1
kendofriendo <goadke@g.cofc.edu>
2021-03-22 07:59:09 -0400

    button styling added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :bands
   resources :gigs
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "application#index"
 end

988ef353de7a21b186d5020c466e10e6c6d61259
kendofriendo <goadke@g.cofc.edu>
2021-03-22 18:36:38 -0400

    General formatting, removed broken code.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
     user.password = Devise.friendly_token[0, 20]
-    user.username = auth.info.username   # assuming the user model has a name
+    user.username = auth.info.nickname   # assuming the user model has a name
     #user.image = auth.info.image # assuming the user model has an image
     # If you are using confirmable and the provider(s) you use validate emails, 
     # uncomment the line below to skip the confirmation emails.
@@ -27,6 +27,7 @@ def self.new_with_session(params, session)
   super.tap do |user|
     if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
       user.email = data["email"] if user.email.blank?
+      user.username = data["nickname"] if user.username.blank?
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :bands
   resources :gigs
-  resources :users, only: [:index,:show]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "application#index"
-
-
 end

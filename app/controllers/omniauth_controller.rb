11ecf1d67b13e106755fd045aabd121c752f7f9e
kendofriendo <goadke@g.cofc.edu>
2021-03-22 09:38:08 -0400

    a

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -4,8 +4,7 @@ class OmniauthController < Devise::OmniauthCallbacksController
 
    def github
      # You need to implement the method below in your model (e.g. app/models/user.rb)
-     @user = User.from_omniauth(request.env["omniauth.auth"])
- 
+     @user = User.create_from_github_data(request.env['omniauth.auth'])
      if @user.persisted?
        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
        set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ def login
   @login || self.username || self.email
 end
 
-  def self.create_from_provider_data(provider_data)
+  def self.create_from_github_data(github_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
-      user.email = provider_data.info.email
+      user.email = github.info.email
       user.password = Devise.friendly_token[0, 20]
       #user.name = auth.info.name   # assuming the user model has a name
       #user.image = auth.info.image # assuming the user model has an image

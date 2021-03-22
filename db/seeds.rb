373a568390741ac845c7021c9fc07c7335b3eb6c
kendofriendo <goadke@g.cofc.edu>
2021-03-22 13:43:57 -0400

    addeded seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do 
+	User.create(
+		username: Faker::Twitter.unique.screen_name
+		email: Faker::Internet.unique.email
+		password: Faker::Internet.password
+	)
+end
+
+10.times do 
+	Band.create(
+		name: Faker::Music.unique.band
+		Faker::Books::Lovecraft.sentence
+		
+	)
+end
+
+# 15.times do
+# 	Gig.create(
+# 		name: Faker::Music.unique.song
+# 		description: Faker::Hipster.paragraph(sentence_count: 2)
+# 	)
+
+end

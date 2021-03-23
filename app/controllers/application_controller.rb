30e6cc8d19abf92eb362a77f1849432cad57743a
kendofriendo <goadke@g.cofc.edu>
2021-03-23 07:53:03 -0400

    changed param code to match example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ end
 
 	def configure_permitted_parameters
 		added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-		devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
-		devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
-		devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
 	end
 end

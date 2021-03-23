376d487795c9001e35c47b8b03b8d4a2f8ff6c1b
kendofriendo <goadke@g.cofc.edu>
2021-03-23 07:53:28 -0400

    added code from username example

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,19 @@ def self.new_with_session(params, session)
     end
   end
 end
+
+def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
+    end
+  end
+end
+
+
 end

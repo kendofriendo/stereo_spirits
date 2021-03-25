99d1b677c006371e1923a3cc1ce8c3c0d235744a
kendofriendo <goadke@g.cofc.edu>
2021-03-24 20:00:18 -0400

    placeholder route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  resources :bands
-  resources :gigs
+  resources :bands, :gigs
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "application#index"
+  get 'profile', to: 'users#show'
 end
+
+
+
+
+=begin
+
+users/id/band
+users/id/band/id
+gig/id
+gig/id/user/id
+
+=end

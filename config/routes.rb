1a24c325218553fd037746f8ac31db97d2ff6ec7
kendofriendo <goadke@g.cofc.edu>
2021-03-28 15:00:00 -0400

    remove gig references

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :band_memberships, only: %i[:show, :index]
-  resources :bands, :gigs
+  resources :band_memberships, only: %i[:index]
+  resources :bands do
+    resources :users, only: %i[:show, :index]
+  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "application#index"
   get 'profile', to: 'users#show'
-  post '/gigs', to: 'gigs#create'
+  post '/bands', to: 'bands#create'
 end
 
 

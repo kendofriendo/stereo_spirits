76e16de11ca92298e7ea74a23c2495175d832e99
kendofriendo <goadke@g.cofc.edu>
2021-03-29 13:53:38 -0400

    added random_user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
+require 'pry'
 module UsersHelper
 	def random_user
-		random_user = User.order(Arel.sql('RANDOM()')).first
+		3.times {|u| [] << User.all.sample.username}
+		ary = [] << User.all.sample.username
+
 	end
+
 end

07c66b256d9bcd354e5bd234bbadaec936597e3c
kendofriendo <goadke@g.cofc.edu>
2021-03-31 02:12:34 -0400

    debug seed

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_202156) do
   create_table "band_memberships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "band_id"
-    t.string "role"
+    t.string "role", default: "band_member"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "joinable"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
 require 'faker'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 def create_admin
 	unless User.find_by(username: "admin")
@@ -23,14 +16,19 @@ end
 		password: Faker::Internet.password
 	)
 end
+
 10.times do 
 	Band.create(
 		name: Faker::Music.unique.band,
 		description: Faker::Books::Lovecraft.sentence
 	)
-	BandMembership.create(
+end
+
+20.times do BandMembership.create(
 		role: Faker::Music::instrument,
 		band_id: Band.ids.sample,
 		user_id: User.ids.sample
 	)
+end
+binding.pry
 end

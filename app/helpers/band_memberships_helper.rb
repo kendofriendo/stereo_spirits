53075bad3837b4d7afa5c70bcdbcf8e3a073d1d9
kendofriendo <goadke@g.cofc.edu>
2021-04-14 11:59:05 -0400

    note, remove pry

--- app/helpers/band_memberships_helper.rb
+++ app/helpers/band_memberships_helper.rb
@@ -1,5 +1,6 @@
 module BandMembershipsHelper
 	def user_role(id)
+			#How can I access this association on the join table and pair it with each user of the band? 
 		BandMembership.where(user_id: id).pick(:role)
 	end
 
